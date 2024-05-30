a64358f7b2750dde6f12732525827493fd44b790
Scorpy22 <jessy19991121@gmail.com>
2024-05-29 22:26:09 -0500

    Clase de formato de cadenas

---
+++ 07_FormatoCadenas/practica1.py
@@ -0,0 +1,10 @@
+"""
+PRACTICA 1
+Necesitamos imprimir el nombre y número de 
+asociado dentro de la siguiente frase: 
+Estimado/a (nombre_asociado), su número de asociado es: (numero_asociado)
+Utilizando el primer metodo
+"""
+nombre = "Pepito"
+numero = 34457
+print("Estimado/a {}, su número de asociado es: {}".format(nombre,numero))
