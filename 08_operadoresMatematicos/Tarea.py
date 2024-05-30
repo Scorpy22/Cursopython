d7137d2e5ba5630e24831607ab3c3883ee9d4bf2
Scorpy22 <jessy19991121@gmail.com>
2024-05-30 18:42:44 -0500

    Operadores Matemáticos

---
+++ 08_operadoresMatematicos/Tarea.py
@@ -0,0 +1,11 @@
+"""
+La situación es esta: tú trabajas en una empresa donde los vendedores reciben comisiones
+del 13% por sus ventas totales, y tu jefe quiere que ayudes a los vendedores a calcular sus
+comisiones creando un programa que les pregunte su nombre y cuánto han vendido en este
+mes. Tu programa le va a responder con una frase que incluya su nombre y el monto que le
+corresponde por las comisiones. 
+"""
+nombre = input("Dime tu nombre: ")
+ventas = input("¿Cuánto has vendido? ")
+
+print("\nTu nombre es: " + nombre + f"\nTu comisión es: {round(0.13*float(ventas),2)}")
