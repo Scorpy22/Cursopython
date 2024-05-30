7a48ca90aee6b0e1586e1b29d632fbd6252a26b0
Scorpy22 <jessy19991121@gmail.com>
2024-05-29 20:55:04 -0500

    Integer y floats

---
+++ 05_integer&Floats/app.py
@@ -0,0 +1,18 @@
+mi_numero = 1
+#print(mi_numero)
+mi_numero2 = 5.8
+
+#verificar el tipo de dato
+#print(type(mi_numero2))
+
+#ejemplo
+#edad = input("Dime tu edad: ")
+#print("Tu edad es " + edad)
+
+#ejemplo2
+edad = input("Dime tu edad: ")
+print("Tu edad es " + edad)
+
+nuevaEdad = edad + edad
+print("Tu edad nueva es: " + nuevaEdad)
+print(type(edad))
