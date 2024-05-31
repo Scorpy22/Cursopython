cd60da182ca9015ecdc435c5b5c0a25c53747eb4
Scorpy22 <jessy19991121@gmail.com>
2024-05-30 22:12:36 -0500

    Operadores Matemáticos

--- 08_operadoresMatematicos/Tarea.py
+++ 08_operadoresMatematicos/Tarea.py
@@ -9,3 +9,10 @@ nombre = input("Dime tu nombre: ")
 ventas = input("¿Cuánto has vendido en este mes? ")
 
 print("\nTu nombre es: " + nombre + f"\nTu comisión es: $ {round(0.13*float(ventas),2)}")
+
+#solucion
+#nombre = input("Por favor, dime tu nombre: ")
+#ventas = float(input("Diga sus ventas totales del mes: "))
+
+#comision = round(ventas * 13 / 100, 2)
+#print(f"Hola {nombre}, tus comisiones de este mes son de ${comision}")
