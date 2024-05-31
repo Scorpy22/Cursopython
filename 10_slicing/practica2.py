3bdc26a4e847b0cfe88b8d390cbc28e76cb82d81
Scorpy22 <jessy19991121@gmail.com>
2024-05-30 22:14:00 -0500

    Clase de slicing

---
+++ 10_slicing/practica2.py
@@ -0,0 +1,7 @@
+""" 
+Practica 2
+Toma cada tercer caracter empezando desde el noveno 
+hasta el final de la frase, e imprime el resultado.
+"""
+frase = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+print(frase[9::3])
