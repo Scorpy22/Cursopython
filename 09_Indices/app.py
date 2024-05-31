1c0f0c85145e554c0638f4c5839571cff48c326c
Scorpy22 <jessy19991121@gmail.com>
2024-05-30 22:13:18 -0500

    Clase de Indices

---
+++ 09_Indices/app.py
@@ -0,0 +1,9 @@
+mi_texto = "Esta es una prueba"
+#resultado = mi_texto [9]
+#print(Resultado)
+
+#resultado = mi_texto.index("prueba", 12 , 18)
+
+# El metodo rindex leer de derecha a izquierda
+resultado = mi_texto.rindex("a") 
+print(resultado)
---
+++ 09_Indices/practica1.py
@@ -0,0 +1,12 @@
+""" 
+PRACTICA 1
+Encuentra y muestra en pantalla qué caracter
+ocupa la quinta posición dentro de la siguiente palabra: "ordenador"
+"""
+
+#palabra = "ordenado"
+#resultado = palabra [5]
+#print("La quinta posición de la palabra es: " + resultado)
+
+palabra = "ordenador"
+print (palabra [5])
