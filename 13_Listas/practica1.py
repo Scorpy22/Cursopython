56251b22da156a40d3fb3cc4c23a288313ead924
Scorpy22 <jessy19991121@gmail.com>
2024-05-31 22:27:30 -0500

    Aprendiendo listas

---
+++ 13_Listas/practica1.py
@@ -0,0 +1,7 @@
+"""
+Practica 1
+Crea una lista con 5 elementos, dentro de 
+la variable mi_lista. Puedes incluir strings, booleanos, números, etc
+"""
+mi_lista = [1, 45, 7, 35, 9]
+#print(type(mi_lista))
---
+++ 13_Listas/practica3.py
@@ -0,0 +1,15 @@
+"""
+Practica 3
+Utiliza el método pop() para quitar el tercer 
+elemento de la siguiente lista llamada frutas, 
+y almacénalo en una variable llamada eliminado. 
+Utiliza métodos de listas sin alterar la línea de código ya suministrada.
+1.manzana
+2.banana
+3.mango
+4.cereza
+5.sandia
+"""
+fruta = ["manzana", "banana", "mango", "cereza", "sandia"]
+eliminado = fruta.pop(2)
+print(fruta)
