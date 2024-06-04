681385fdf5009f527c730b161e0d8809f3acfc45
Scorpy22 <jessy19991121@gmail.com>
2024-06-03 22:49:06 -0500

    Controles de flujo

---
+++ 20_OperadoresLogicos/practica2.py
@@ -0,0 +1,21 @@
+"""
+Practica 2.
+
+Verifica si las palabras almacenadas en las siguientes variables:
+
+palabra1 = "éxito", y
+palabra2 = "tecnología"
+
+no se encuentran en la frase a continuación, y almacena 
+el resultado de esta comprobación (un booleano) en una variable llamada mi_bool:
+
+"Cuando algo es lo suficientemente importante, lo haces incluso 
+si las probabilidades de que salga bien no te acompañan"
+
+Elon Musk
+"""
+palabra1 = "éxito"
+palabra2 = "tecnología"
+frase = "Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan"
+mi_bool = (palabra1 not in frase) and (palabra2 not in frase)
+print(mi_bool)
