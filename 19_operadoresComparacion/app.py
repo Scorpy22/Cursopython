c029fe65997f9f53390f9a6690a055954f9365ee
Scorpy22 <jessy19991121@gmail.com>
2024-06-03 22:48:10 -0500

    Operadores de comparacion

---
+++ 19_operadoresComparacion/app.py
@@ -0,0 +1,11 @@
+mi_variable = 'Hola mundo'
+resultado = mi_variable == "Chao mundo"
+print(resultado)
+
+mi_bool = 10 == 25
+mi_bool = '100' == 100
+mi_bool = 100.0 == 100
+mi_bool = 100 != 100
+mi_bool = 10 < 20
+mi_bool = 5 >= 7
+print(mi_bool)
