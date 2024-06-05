0beaf8658e44e39d63740af84939fded8e3d8576
Scorpy22 <jessy19991121@gmail.com>
2024-06-04 22:21:03 -0500

    Loop For

---
+++ 22_LoopFor/app.py
@@ -0,0 +1,33 @@
+#lista = ['a','b','c','d']
+
+#for item in lista:
+#    numero_letra = lista.index(item)
+#    print(f"letra {numero_letra}:{item}")
+#lista = ['a','b','c','d']
+#for item in lista:
+#  numero_letra = lista.index(item) + 1
+#  print(f"letra {numero_letra}: {item}")
+
+#otro ejemplo
+#lista=['pablo', 'laura', 'jose', 'luis', 'julia']
+#for item in lista:
+#    if item.startswith('l'):
+#        print(item)
+#    else:
+#        print("Nombre que no comienzan con L")
+
+#otro ejemplo
+#numero = [1,2,3,4,5]
+#mi_valor = 0
+#for item in numero:
+#    mi_valor = mi_valor + item
+#    print(mi_valor)
+
+#directamente
+#for a,b in [[11,2],[2,3],[5,6]]:
+#    print(f"{a} - {b}")
+
+#diccionarios
+dic = {'clave1':'a','clave2':'b','clave3':'c'}
+for a,b in dic.items():
+    print(a,b)
