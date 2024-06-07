f55fda829815d51c2b6229a7f14521c20bd6570e
Scorpy22 <jessy19991121@gmail.com>
2024-06-06 22:28:03 -0500

    Funciones

--- 30_Funciones/funcionesReturn.py
+++ 30_Funciones/funcionesReturn.py
@@ -24,6 +24,7 @@ def factorial(n):
     if n == 0:
         return 1
     else:
+        #aqui se aplica la recursividad factorial que se llama asi mismo
         return n*factorial(n-1)
 
 #llamada a la funcion
